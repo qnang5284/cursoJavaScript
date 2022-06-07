3455fc4bafa1a73cad0fe41438bccf9a89668180
JaviS <qnang5284@gmail.com>
2022-06-07 16:35:28 +0200

    first assignment done

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,14 @@
+let userInput = 15;
+let result;
+
+result = userInput + 18;
+
+result = result - 3;
+
+result = result * 2;
+
+result = result / 2;
+
+alert(`userInput = ${userInput}`);
+alert(`result = ${result}`);
+
