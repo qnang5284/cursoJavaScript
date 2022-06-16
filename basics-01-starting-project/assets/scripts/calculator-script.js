f61ba09bcd226a27112d3a48a807083948a4bc0f
JaviS <qnang5284@gmail.com>
2022-06-16 14:47:26 +0200

    finished stable calculator working wit floats

--- basics-01-starting-project/assets/scripts/calculator-script.js
+++ basics-01-starting-project/assets/scripts/calculator-script.js
@@ -1,12 +1,57 @@
 // declare constants with "const"
-const defaultResult = 0; 
+const DEFAULT_RESULT = 0; 
 // declare variables with "let"
-let currentResult = defaultResult;
+let currentResult = DEFAULT_RESULT;
 
-currentResult = currentResult + 10;
+const DEFAULT_CALCULATION = '';
+let currentCalculation = DEFAULT_CALCULATION;
 
-let calculationDesc = `${defaultResult} + 10`;
-// Which is similar as
-// let calculationDesc = defaultResult + " + 10";
 
-outputResult(currentResult, calculationDesc);
+function add() {
+    let inputVal = getInput();
+    currentCalculation = getSymbolToCurrentCalculation("+", inputVal);
+    currentResult = currentResult + inputVal;
+    outputResult(currentResult, currentCalculation);
+    emptyCurrentCalculation();
+}
+addBtn.addEventListener('click', add);
+
+function substract() {
+    let inputVal = getInput();
+    currentCalculation = getSymbolToCurrentCalculation("-", inputVal);
+    currentResult = currentResult - inputVal;
+    outputResult(currentResult, currentCalculation);
+    emptyCurrentCalculation();
+}
+subtractBtn.addEventListener('click', substract);
+
+function multiply() {
+    let inputVal = getInput();
+    currentCalculation = getSymbolToCurrentCalculation("*", inputVal);
+    currentResult = currentResult * inputVal;
+    outputResult(currentResult, currentCalculation);
+    emptyCurrentCalculation();
+}
+multiplyBtn.addEventListener('click', multiply);
+
+function divide() {
+    let inputVal = getInput();
+    currentCalculation = getSymbolToCurrentCalculation("/", inputVal);
+    currentResult = currentResult / inputVal;
+    outputResult(currentResult, currentCalculation);
+    emptyCurrentCalculation();
+}
+divideBtn.addEventListener('click', divide);
+
+function getInput() {
+    return parseFloat(userInput.value);
+}
+
+function getSymbolToCurrentCalculation(operator, userInputValue) {
+    return currentCalculation + currentResult + operator + userInputValue; 
+}
+
+function emptyCurrentCalculation(){
+    userInput.value = '';
+    currentCalculation = DEFAULT_CALCULATION;
+}
